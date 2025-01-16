52f13a556dcc49236cfe0960d2111f94c017c7bb
fhtgrjlirtjgv <maksikdonlove001@gmail.com>
2025-01-16 20:32:39 +0200

    123

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+from models import *
 
 app = Ursina()
 
 sky = Sky()
-ground = Entity(model='plane',collider='box', scale=300, texture='grass', texture_scale=(4,4), position=(0,-2,0))
+
+map = Map()
+map.genetator()
+
+
 player = FirstPersonController()
-
-
-block = Entity(model='cube', texture='grass', position=(2,5,0), scale=0.5)
-
 app.run()
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from ursina import *
-
+from perlin_noise import PerlinNoise
+from numpy import floor
+from ursina.shaders import basic_lighting_shader
 
 class Block(Entity):
     def __init__(self, block_type, pos, **kwargs):
@@ -12,7 +14,28 @@ class Block(Entity):
             collider='box',
             origin_y=-0.5,
             color=color.color(0,0, random.uniform(0.9, 1)),
+            shader=basic_lighting_shader,
             **kwargs
+        
         )
+
+
+class Map(Entity):
+    def __init__(self, **kwargs):
+        super().__init__(model=None, collider=None, **kwargs)
+        self.bedrock = Entity(model='plane',collider='box', scale=300, texture='grass', texture_scale=(4,4), position=(0,-2,0))
+        self.blocks = {}
+        self.noise = PerlinNoise(octaves=2, seed=1488)
+
+    def genetator(self):
+        for x in range(50):
+            for z in range(50):
+                y = floor(self.noise([x/24, z/24])*6)
+                block = Block(0, (x,y,z))
+
+
+
+
+
 
 
